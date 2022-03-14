57bc35f38227d74aa12e45eb6da6b22c7e1740fb
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-14 13:17:54 -0700

    test student fields return correct inputs

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,13 +1,25 @@
 import java.util.ArrayList;
 
 public class Student {
-    private long studentId;
+    private long studentID;
     private String studentName;
-    private ArrayList<Integer> studentGrades;
+    private ArrayList<Integer> grades;
 
-    public Student(long studentId, String studentName) {
-        this.studentId = studentId;
+    public Student(long studentID, String studentName) {
+        this.studentID = studentID;
         this.studentName = studentName;
-        this.studentGrades = new ArrayList<>();
+        this.grades = new ArrayList<>();
+    }
+
+    public long getID() {
+        return this.studentID;
+    }
+
+    public String getName() {
+        return this.studentName;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 public class StudentTest {
 
     @Test
-    public void testCreatStudent() {
+    public void testCreateStudent() {
         Student Obj = new Student(133, "Odell");
         Student ObieJ = null;
         assertNull(ObieJ);
         assertNotNull(Obj);
+    }
+
+    @Test
+    public void testStudentFields() {
+        Student Jarvis = new Student(80, "Jarvis");
+        assertEquals("Jarvis", Jarvis.getName());
+        assertEquals(80, Jarvis.getID());
+        assertEquals(0, Jarvis.getGrades().size());
     }
 }
